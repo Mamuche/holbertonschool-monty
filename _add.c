8d61f253b85df474ec67901097bf014698fe25e0
Mamuche <marionlaroche17@gmail.com>
2024-01-03 14:47:29 +0100

    fix _pop create _add

---
+++ _add.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+
+/**
+ * _add - adds the top two elements of the stack.
+ * @stack: the stack
+ * @line_number: line number
+*/
+
+void _add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+	int sum = 0;
+
+	if (!*stack || !(*stack)->next)
+	{
+		dprintf(2, "L%u: can't add, stack too short\n", line_number);
+		free(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = *stack;
+	sum = tmp->next + tmp->next->n;
+	tmp->next->n = sum;
+	_pop(stack, line_number);
+}
