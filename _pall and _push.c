5fc871601605226104c2a3afcd6b0f4b7ba88df3
emma-33 <emma.lateyron@orange.fr>
2024-01-04 15:13:55 +0100

    modif _push and _pall

--- _pall and _push.c
+++ _pall and _push.c
@@ -1,4 +1,25 @@
 #include "monty.h"
+
+/**
+* _pall - prints all the values of the stack
+* @stack: the stack
+* @line_number: line number
+* @value: value
+*/
+void _pall(stack_t **stack, unsigned int line_number, char *value)
+{
+	(void)line_number;
+	(void)value;
+	stack_t *node = *stack;
+
+	while (node != NULL)
+	{
+		fprintf(stdout, "%d\n", node->n);
+		node = node->next;
+	}
+}
+
+#include "monty.h"
 /**
 * _push - pushes an element to the stack
 * @stack: the stack
